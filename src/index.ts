d4e5e1bba884a3839c1caffee64eca136fcaf5a1
孟世博 <897205285@qq.com>
2023-01-28 14:54:57 +0800

    feat: add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
-import TelegramBot from 'node-telegram-bot-api'
+import TelegramBot from "node-telegram-bot-api";
 import { config } from "dotenv";
-config()
+config();
 
-// replace the value below with the Telegram token you receive from @BotFather
-
-const token = process.env.TELEGRAM_TOKEN || '';
-const chatId = process.env.TELEGRAM_CHAT_ID || '';
-
-console.log("process.env", process.env);
+const token = process.env.TELEGRAM_TOKEN || "";
+const chatId = process.env.TELEGRAM_CHAT_ID || "";
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", (msg) => {
-    setInterval(() => {
-        const date = new Date(0);
-        bot.sendMessage(chatId, `Received your message${date}`);
-    }, 5000);
+  setInterval(() => {
+    const date = new Date(0);
+    bot.sendMessage(chatId, `Received your message${date}`);
+  }, 5000);
 });
