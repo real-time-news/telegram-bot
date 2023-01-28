39040c3f904b5022c01d39c32d7c583a900aac3f
孟世博 <897205285@qq.com>
2023-01-28 15:04:38 +0800

    feat: github action

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,12 @@ const chatId = process.env.TELEGRAM_CHAT_ID || "";
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", (msg) => {
-  setInterval(() => {
-    const date = new Date(0);
-    bot.sendMessage(chatId, `Received your message${date}`);
-  }, 5000);
-});
+// 启动时发送消息;
+bot.sendMessage(chatId, "Hello World");
+
+// bot.on("message", (msg) => {
+//   setInterval(() => {
+//     const date = new Date(0);
+//     bot.sendMessage(chatId, `Received your message${date}`);
+//   }, 5000);
+// });
