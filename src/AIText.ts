6f507026aae844d772e2f41eeb8987944567eb14
孟世博 <897205285@qq.com>
2023-02-09 16:21:15 +0800

    Update AIText.ts

--- src/AIText.ts
+++ src/AIText.ts
@@ -8,23 +8,27 @@ const AIText = async (prompt?: string) => {
 
   if (!prompt) return Promise.resolve("");
 
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `${prompt} 请一句话评价这条新闻`,
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    max_tokens: 1000,
-    stream: false,
-    n: 1,
-  });
-
-  const { data } = completion || {};
-  const { choices } = data || [];
-  const { text } = choices[0] || {};
-  const trimText = text?.trim();
-  return trimText || "";
+  //捕获错误
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt} 请一句话评价这条新闻`,
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 1000,
+      stream: false,
+      n: 1,
+    });
+    const { data } = completion || {};
+    const { choices } = data || [];
+    const { text } = choices[0] || {};
+    const trimText = text?.trim();
+    return trimText || "";
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default AIText;
